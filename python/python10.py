06c268d178168cf9eb290aaa3165508556b06420
Darrehan <darrehanrasool@gmail.com>
2024-01-31 17:44:31 +0530

    Dar Rehan Rasool
    sycomusiclover

--- python/python10.py
+++ python/python10.py
@@ -1,9 +1,11 @@
 # coding for random module
 import random
+
+from sympy import N
 names=["rehan","arman","sultan",  "khan","arif"]
 total_size=len(names)
 # here list index start from 0 to 4
 random_number=random.randint(0,total_size-1)
 print(f"{names[random_number]} is going to buy the meal today!")
-for name in range(0,total_size):
+for name  in names:
     print(name)
