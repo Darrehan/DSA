1322de59e96f547de151eb38e88216aa8361ea4b
Darrehan <darrehanrasool@gmail.com>
2024-09-12 14:15:23 +0530

    Dar Rehan Rasool
    sycomusiclover

--- java/first.java
+++ java/first.java
@@ -1,7 +1,5 @@
-public class first {
-
-    public static void main(String args[]) {
-        System.out.println("Hello World");
+public class first{
+    public static void main (String args[]){
+        System.out.println("hello world ");
     }
-
 }
